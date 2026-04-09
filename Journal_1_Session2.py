d3d5b76cb35c8a0e7bc5dc1992d5e8b286b34a82
gjcampos-sketch <gjcampos@ucsc.edu>
2026-04-08 18:19:09 -0700

    Add files via upload

---
+++ Journal_1_Session2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 18:05:49 2026
+
+@author: theja
+"""
+a=float(6.2)
+b=float(6.5)
+print(a+b)
+Addition_Result= a+b
+print(type(Addition_Result))
+
+c= 5
+d= 9
+print(c-d)
+Subtraction_Result= c-d
+print(type(Subtraction_Result))
+
+print(a*d)
+Product_Result= a*d
+print(type(Product_Result))
