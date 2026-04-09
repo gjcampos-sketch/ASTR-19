c5d906b3a376a496fb0a9fb3cb5d056f18a59954
gjcampos-sketch <gjcampos@ucsc.edu>
2026-04-08 17:59:08 -0700

    Add files via upload

---
+++ Journal_1_Session1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:49:40 2026
+
+@author: theja
+"""
+
+my_name= "Hello my name is Gabriel Campos."
+print(my_name)
+favorite_food= "My favorite food would have to be nice creamy Mac N' Cheese."
+print(favorite_food)
+favorite_movie= "My favorite movie of all time is Rocky II!"
+print(favorite_movie)
