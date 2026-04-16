db8ca15e315ef1ea0461a976bdd258dad2fb7f5c
gjcampos-sketch <gjcampos@ucsc.edu>
2026-04-15 17:55:04 -0700

    Add files via upload

---
+++ Journal_1_Session3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 17:28:26 2026
+
+@author: theja
+"""
+def f(x):
+    return x**3 + 8
+def main():
+    x = 9
+    result = f(x)
+    print (result)
+    if result > 27:
+        print("YAY!")
+
+
+if __name__=='__main__':
+    main()
+    
